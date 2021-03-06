bfd7b03e22f5785a366552d5925d8a57d0c65344
TravisSLee <travisslee223@gmail.com>
2021-03-06 12:35:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+class Polygon {
+    constructor(arr){
+        this.arr = arr
+    }
+
+    get countSides(){
+        return this.arr.length
+    }
+
+    get perimeter(){
+        const reducer = (accumulator, currentValue) => accumulator + currentValue
+        let total = this.arr.reduce(reducer)
+        return total
+    } 
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        return (this.countSides === 3 && 
+            this.arr[0] + this.arr[1] > this.arr[2] &&
+            this.arr[1] + this.arr[2] > this.arr[0] &&
+            this.arr[2] + this.arr[0] > this.arr[1])
+    }
+}
