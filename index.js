6fff3dc5268cfb09171d18f9d771255a25df4a2e
TravisSLee <travisslee223@gmail.com>
2021-03-06 12:40:45 -0500

    Done.

--- index.js
+++ index.js
@@ -22,3 +22,13 @@ class Triangle extends Polygon {
             this.arr[2] + this.arr[0] > this.arr[1])
     }
 }
+
+class Square extends Polygon {
+    get isValid(){
+        return (this.countSides === 4 && this.arr[0] === this.arr[1] && this.arr[2] === this.arr[1] && this.arr[2] === this.arr[3] )
+    }
+
+    get area(){
+        return this.arr[0] * this.arr[1]
+    }
+}
